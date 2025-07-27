6a464bfdb61ad95b5d1bc78939a1547f79dddc30
tdziewa08 <141276940+tdziewa08@users.noreply.github.com>
2025-07-27 13:12:50 -0400

    added react-router-dom
    installed "react-router-dom" as a dependency
    
    moved components into individual <Route /> components
    
    added a rough navigation bar to render with each component path

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./Home.jsx"
 import Members from './Members.jsx'
 import Stickers from './Stickers.jsx'
 import About from "./About.jsx"
+import Layout from "./components/Layout.jsx"
+
 
 export default function App() {
   return (
-    <>
-      <h1>Testing</h1>
-      <Members />
-      <Stickers />
-      <About />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/members" element={<Members />} />
+          <Route path="/stickers" element={<Stickers />} />
+          <Route path="/about" element={<About />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+export default function Header() {
+    return (
+        <header className="header">
+			<Link to="/">Home</Link>
+            <Link to="/members">Members</Link>
+            <Link to="/stickers">Stickers</Link>
+			<Link to="/about">About</Link>
+		</header>
+    )
+}
