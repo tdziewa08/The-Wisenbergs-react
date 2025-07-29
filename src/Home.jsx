def5ca051574974dce750b28259f891655414813
tdziewa08 <141276940+tdziewa08@users.noreply.github.com>
2025-07-29 19:00:00 -0400

    added pseudo-code
    wrote out pseudo code in the app
    
    adjusted css styling

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,8 +2,17 @@ import React from "react"
 
 export default function Home() {
     return (
-        <>
-            <h1>HOME PAGE</h1>
-        </>
+        <div className="home-container">
+            <h1>Welcome to The Wisenberg's team website!</h1>
+            <div className="team-img-container">
+                <img src="https://www.iwastesomuchtime.com/wp-content/uploads/sites/68/2021/12/9272013181733-22268.jpg" alt="The Wisenberg's team photo"/>
+            </div>
+            <p>
+                We are The Wisenberg's trivia team! We compete in trivia contests, twice a month, throughout Gloucester County. Our mission is simple, win one.
+            </p>
+            <p>
+                Total Wins: 0
+            </p>
+        </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,44 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+//Pseudo Code for App
+/*
+  Home, Members, Stickers, About pages
+
+  HOME PAGE
+    - team picture
+    - team anecdote
+    - countdown clock until next trivia event
+    - other images
+    - tony links?
+
+  MEMBERS PAGE
+    - actual pictures of members
+    - member authored / aapproved descriptions
+    - less deliberate / rectangular layout
+  
+  STICKERS PAGE
+    - image grid of all stickers received over the years
+    - descriptions of the stickers on the back when hovered over
+    - description of the page
+  
+  ABOUT PAGE
+    - fun facts about the team
+    - description of the team
+    - team statistics
+    - testimonials
+    - extra images
+
+  SCHEDULE PAGE???
+    - implement google calendar or other API to display the list of events we plan to attend
+    - good styling
+    - make sure the calendar has permissions allowed and is always visible and accessible to the user
+
+  
+  - Stickers scanned and stored in google firestore database
+  - set it up to allow easy additions of new stickers for me
+
+  - improve styling through-out the app
+  - better fonts
+*/
