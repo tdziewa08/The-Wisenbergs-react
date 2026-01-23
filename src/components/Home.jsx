f478fb4dec66d8d841c87383800e926012569e05
tdziewa08 <141276940+tdziewa08@users.noreply.github.com>
2026-01-23 11:46:07 -0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import theTeam from "../assets/new-member-images/team.jpg"
 
 export default function Home() {
 
-    const triviaDate = new Date('January 20, 2026 19:00:00');
+    const triviaDate = new Date('January 27, 2026 19:00:00');
  
     return (
         <div className="home-container">
