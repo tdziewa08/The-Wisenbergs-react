0685110c548c353c2533125d18c975ac95e8a19b
tdziewa08 <141276940+tdziewa08@users.noreply.github.com>
2026-06-19 12:14:02 -0400

    added mobile device query

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import CountdownClock from "./CountdownClock"
 
 export default function Header() {
 
-    const triviaDate = new Date('June 18, 2026 19:00:00');
+    const triviaDate = new Date('June 30, 2026 19:00:00');
 
     return (
         <>
